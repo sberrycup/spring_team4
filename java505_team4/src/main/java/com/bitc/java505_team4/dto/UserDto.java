76531ad05d08dd2829c5c6fae35c39c5af2166ff
DESKTOP-MC0FHUI\kaon7 <kaon715@naver.com>
2023-07-02 21:41:08 +0900

    no message

---
+++ java505_team4/src/main/java/com/bitc/java505_team4/dto/UserDto.java
@@ -0,0 +1,13 @@
+package com.bitc.java505_team4.dto;
+
+import lombok.Data;
+
+@Data
+public class UserDto {
+    private int memberNum;
+    private String memberEmail;
+    private String memberPw;
+    private String memberName;
+
+//    private String adminYn;
+}
